cd188e8dc194657a92d3dd38018e7cb710b6b46f
Mohammad Firoj <firojmd59313@gmail.com>
2026-04-17 16:59:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scroll for Internal Links
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+  link.addEventListener("click",e=>{
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({behavior:"smooth"});
+  });
+});
